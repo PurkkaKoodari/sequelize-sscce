908e2ada2fac9f733bff3bf1c1a8863b1f174c06
PurkkaKoodari <purkka@purkka.codes>
2021-09-18 18:21:18 +0300

    SSCCE for TypeScript 4.4 breakage

--- src/sscce.ts
+++ src/sscce.ts
@@ -1,40 +1,26 @@
-// Require the necessary things from Sequelize
-import { Sequelize, Op, Model, DataTypes } from 'sequelize';
+import {DataTypes, Model, Op, Sequelize} from 'sequelize';
 
-// This function should be used instead of `new Sequelize()`.
-// It applies the config for your SSCCE to work on CI.
-import createSequelizeInstance = require('./utils/create-sequelize-instance');
+const sequelize: Sequelize = null as any;
 
-// This is an utility logger that should be preferred over `console.log()`.
-import log = require('./utils/log');
-
-// You can use sinon and chai assertions directly in your SSCCE if you want.
-import sinon = require('sinon');
-import { expect } from 'chai';
-
-// Your SSCCE goes inside this function.
-export async function run() {
-  const sequelize = createSequelizeInstance({
-    logQueryParameters: true,
-    benchmark: true,
-    define: {
-      timestamps: false // For less clutter in the SSCCE
-    }
-  });
-
-  class Foo extends Model {};
-  Foo.init({
-    name: DataTypes.TEXT
-  }, {
-    sequelize,
-    modelName: 'Foo'
-  });
-
-  const spy = sinon.spy();
-  sequelize.afterBulkSync(() => spy());
-  await sequelize.sync();
-  expect(spy).to.have.been.called;
-
-  log(await Foo.create({ name: 'TS foo' }));
-  expect(await Foo.count()).to.equal(1);
+interface PostAttributes {
+  authorId: number;
 }
+
+class Post extends Model<PostAttributes> implements PostAttributes {
+  public authorId!: number;
+};
+
+Post.init({
+  authorId: DataTypes.INTEGER,
+}, {
+  sequelize,
+});
+
+// https://sequelize.org/master/manual/model-querying-basics.html#the-basics
+Post.findAll({
+  where: {
+    authorId: {
+      [Op.eq]: 2
+    }
+  }
+});
